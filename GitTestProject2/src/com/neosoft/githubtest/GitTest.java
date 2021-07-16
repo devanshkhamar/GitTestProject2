cdb08c8d8633d352f3fa0c1414338be5739ff58c
devanshkhamar <devanshkhamar@gmail.com>
2021-07-16 18:58:48 +0530

    First Commit

---
+++ GitTestProject2/src/com/neosoft/githubtest/GitTest.java
@@ -0,0 +1,16 @@
+package com.neosoft.githubtest;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		String code = "SE";
+		if(code == "SE")
+			System.out.println("Software Engineer");
+		else 
+			System.out.println("Not");
+			
+		
+
+	}
+
+}
